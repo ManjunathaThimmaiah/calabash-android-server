7dc3aedeb9745933523957536f73acad8d12564b
Manjunatha Thimmaiah <manjunatha.thimmaiah@xing.com>
2023-01-09 09:46:20 +0100

    Refactoring the code

--- server/app/src/androidTest/java/androidx/test/uiautomator/UiScrollableCustom.java
+++ server/app/src/androidTest/java/androidx/test/uiautomator/UiScrollableCustom.java
@@ -7,27 +7,52 @@ public class UiScrollableCustom extends UiScrollable {
     public UiScrollableCustom(UiSelector container) {
         super(container);
     }
-    Long TIMEOUT = 1000L;
+
+    private static final Long TIMEOUT = 1000L;
+
+    public boolean scrollIntoView(UiSelector selector) throws UiObjectNotFoundException {
+        return scrollIntoView(selector, false);
+    }
 
     /**
      * Method required because scrollForward return type is not reliable. It might return false, meaning that there is no
      * more space to scroll while it is not true. This method allows you to force to scroll even if scrollForward returns
      * false. When forcing to scroll, the method will stop scrolling when mMaxSearchSwipes is reached.
      */
-    public boolean scrollIntoView(UiSelector selector, String bystrategy, boolean forceScroll) throws UiObjectNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+    public boolean scrollIntoView(UiSelector selector, boolean forceScroll) throws UiObjectNotFoundException {
         Tracer.trace(new Object[]{selector});
         UiSelector childSelector = this.getSelector().childSelector(selector);
+        boolean found = false;
+
+        for (int x = 0; x < getMaxSearchSwipes() && !found; ++x) {
+            if (this.exists(childSelector)) {
+                found = true;
+            } else {
+                boolean scrolled = this.scrollForward(100);
+                getDevice().waitForIdle();
+                if (!forceScroll && !scrolled) break;
+            }
+        }
+        return found;
+    }
+
+    public boolean scrollIntoView(UiSelector selector, String targetBySelectorStrategy, boolean forceScroll) throws UiObjectNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InvocationTargetException {
+        Tracer.trace(new Object[]{selector});
+        UiSelector childSelector = this.getSelector().childSelector(selector);
+        Method strategyMethod = By.class.getMethod(targetBySelectorStrategy, String.class);
+        BySelector byselector = (BySelector) strategyMethod.invoke(By.class, (String.valueOf(childSelector)));
         boolean found = false;
         if (this.exists(childSelector)) {
             found = true;
         }else {
-            for (int x = 0; x < getMaxSearchSwipes() && !found; ++x) {
+            for (int x = 0; x < getMaxSearchSwipes(); ++x) {
                 boolean scrolled = this.scrollForward(50);
                 getDevice().waitForIdle();
-                Method strategyMethod = By.class.getMethod(bystrategy, String.class);
-                getDevice().wait(Until.findObject((BySelector) strategyMethod.invoke(By.class, (String.valueOf(childSelector)))), TIMEOUT);
-                found = true; if (this.exists(childSelector));
-                if (!forceScroll && !scrolled && found) break;
+                getDevice().wait(Until.findObject(byselector), TIMEOUT);
+                if (this.exists(childSelector)) {
+                    found = true; break;
+                };
+                if (!forceScroll && !scrolled) break;
             }
         }
         return found;
